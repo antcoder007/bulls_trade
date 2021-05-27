5c78d57821ed61b1678febe37ea60bd3ab11b2de
Premanshu Yadav <contact@premanshu.com>
2021-05-27 18:07:18 -0400

    added login post & get functionality

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 
+const auth = require("./../middleware/auth");
 const User = require("../model/User");
 
 /**
@@ -17,7 +18,7 @@ const User = require("../model/User");
     [
         // check for valid username
         check("username", "Please enter a valid username").notEmpty(),
-        // check for valid password
+        // check for valid email
         check("email", "Please enter a valid email address").isEmail(),
         // check for valid password
         check("password", "Please enter a valid password").isLength({
@@ -83,5 +84,87 @@ const User = require("../model/User");
         }
     }
 );
+
+/**
+ * @method - POST
+ * @param - /login
+ * @description - User Login
+ */
+router.post(
+    "/login",
+    [        
+    // check for valid email
+    check("email", "Please enter a valid email").isEmail(),
+    // check for valid password
+    check("password", "Please enter a valid password").isLength({
+        min: 6
+      })
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+  
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array()
+        });
+      }
+  
+      const { email, password } = req.body;
+      try {
+        let user = await User.findOne({
+          email
+        });
+        if (!user)
+          return res.status(400).json({
+            message: "User does not exist!"
+          });
+  
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch)
+          return res.status(400).json({
+            message: "Incorrect Password!"
+          });
+  
+        const payload = {
+          user: {
+            id: user.id
+          }
+        };
+  
+        jwt.sign(
+          payload,
+          "randomString",
+          {
+            expiresIn: 3600
+          },
+          (err, token) => {
+            if (err) throw err;
+            res.status(200).json({
+              token
+            });
+          }
+        );
+      } catch (e) {
+        console.error(e);
+        res.status(500).json({
+          message: "Server Error"
+        });
+      }
+    }
+  );  
+
+/**
+ * @method - GET
+ * @description - Get LoggedIn User
+ * @param - /user/me
+ */
+ router.get("/me", auth, async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id);
+      res.json(user);
+    } catch (e) {
+      res.send({ message: "Cannot fetch user..." });
+    }
+  });
 
 module.exports = router;
