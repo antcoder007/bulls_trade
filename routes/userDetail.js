01c59c49be42ab09ffffa6bb989e40e2602409b0
Premanshu Yadav <contact@premanshu.com>
2021-06-15 23:34:01 -0400

    add to wishlist endpoint active

--- routes/userDetail.js
+++ routes/userDetail.js
@@ -7,6 +7,7 @@ const router = express.Router();
 const auth = require("./../middleware/auth");
 const User = require("../model/User");
 const UserDetail = require("../model/UserDetail");
+const Product = require("../model/Product");
 
 /**
  * @method : *
@@ -49,5 +50,45 @@ const UserDetail = require("../model/UserDetail");
       res.send({ message: "Cannot fetch prodcuts..." });
     }
   });
+
+  /**
+ * @method : POST
+ * @param : /watch
+ * @description : Add to watchlist
+ */
+ router.post(
+  "/watch", auth, async (req, res) => {
+    
+      // get id of loggedIn user
+      const createdBy = req.user.id;
+      // console.log("userId is: ", createdBy);
+    try{  
+      /**
+       * get product id from body =>
+       * ____Get this id from `user_selected` product
+       * on front-end and send in through request
+       * @headers i.e. id: __
+       */
+        const product_id = req.body.id;
+      // find user detail document
+        const detail = await UserDetail.findOne({userId: createdBy});
+      // find product document by id
+        const product = await Product.findOne({productId: product_id})
+      // push products in user wishlist
+        detail.watchList.push(product);
+      // save new user detail
+        await detail.save();
+        res.status(200).json({
+          message: "The product was stored in wishlist."
+      });
+
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({
+        message: "Server Error"
+      });
+    }
+  }
+);  
 
   module.exports = router;
