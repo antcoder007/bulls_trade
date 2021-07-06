13c37055fa4174f9d27d1334640ba723bb410b2f
Premanshu Yadav <contact@premanshu.com>
2021-07-06 10:27:12 -0700

    dynamic header for different pages

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -6,15 +6,47 @@ class Header extends Component {
   constructor(props) {
     super(props);
   }
+  
   render() {
     const path = this.props.location.pathname.slice(1);
-    return (
-    <nav class="navbar navbar-dark bg-dark">
-        <div className="row col-2 d-flex justify-content-center text-white">
-        <span className="h5"><Text style={{textTransform: 'capitalize', color: 'white'}}>{path}</Text></span>
-        </div>
-    </nav>
-    );
+    console.log("the path is: " + path);
+    if (path === 'profile' || path === 'products') {
+      console.log("are you here?");
+      return (
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <span class="navbar-brand">{path}</span>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="/products">Home</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href='#' id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Profile
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="/profile">My Profile</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Logout</a>
+                </div>
+              </li>
+            </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+      );
+    } else {
+      return (
+      <nav class="navbar navbar-dark bg-dark">
+          <div className="row col-2 d-flex justify-content-center text-white">
+            <span className="h5"><Text style={{textTransform: 'capitalize', color: 'white'}}>{path}</Text></span>
+          </div>
+      </nav>
+      );
+    }
   }
 }
 
