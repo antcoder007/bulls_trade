f2757e57e2b9f0153ada86a6a087baed6be0c262
Premanshu Yadav <contact@premanshu.com>
2021-07-06 10:27:51 -0700

    minor fixes

--- client/src/components/Home/BaseHome.js
+++ client/src/components/Home/BaseHome.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import './BaseHome.css';
 //import { useHistory } from 'react-router-dom';
 
 class BaseHome extends Component {
