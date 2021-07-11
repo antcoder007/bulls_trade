72b9aaa66d87080414eecd1374e5249cb8be9f7e
Premanshu Yadav <contact@premanshu.com>
2021-07-11 12:41:29 -0700

    enable navbar toggling

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -19,8 +19,11 @@ class Header extends Component {
       console.log("are you here?");
       return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
           <span class="navbar-brand">{path}</span>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
                 <a class="nav-link" href="/products">Home</a>
