7171c0d38a731940c80c2a382a13c5aa65be2931
Premanshu Yadav <contact@premanshu.com>
2021-07-22 00:54:00 -0400

    chore

--- client/src/components/Product/ProductItem.js
+++ client/src/components/Product/ProductItem.js
@@ -1,6 +1,35 @@
 import React, { useEffect, useState } from "react";
+import AuthService from "../../services/auth.service";
 
 const ProductItem = props => {
+
+    // const [success, setSuccess] = useState("");
+    // const [message, setMessage] = useState("");
+
+    // const handleWishlist = (e) => {
+    //     e.preventDefault();
+    //     setSuccess("");
+    //     setMessage("");
+        
+    //     AuthService.createWishProduct().then(
+    //         () => {
+    //             setSuccess("Your Product was Added to the wishlist!")
+    //             setTimeout(function(){}, 2000); 
+    //             window.location.reload();
+    //         },
+    //         (error) => {
+    //             const resMessage =
+    //                 (error.response &&
+    //                 error.response.data &&
+    //                 error.response.data.message) ||
+    //                 error.message ||
+    //                 error.toString();
+    //             setMessage(resMessage);
+    //           }
+    //     );
+
+    //   };
+
   const { product } = props;
   return (
     <div class="col-md-4 col-xs-6">
@@ -10,6 +39,7 @@ const ProductItem = props => {
                 <h4 class="card-title">{product.productName}</h4>
                 <p class="card-text">{product.productDescription}</p>
                 <a href="#" class="btn btn-dark">Add to Cart</a>
+                <a href="#" class="btn btn-dark float-right">Add to Wishlist</a>
             </div>
             <div class="card-footer">
                 {product.isAvailable === true ? (
