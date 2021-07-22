7be01cd0c9c21a01a6b4dc74064216037316250f
Premanshu Yadav <contact@premanshu.com>
2021-07-22 00:53:17 -0400

    added cart button

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -38,6 +38,9 @@ class Header extends Component {
                 <a class="dropdown-item" href="/" onClick={() => this.logout()}>Logout</a>
                 </div>
               </li>
+              <button type="button" class="btn btn-info">
+                Cart 
+              </button>
             </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
